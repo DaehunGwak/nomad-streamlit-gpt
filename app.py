55d967e97d9ced976f196cecc6619f7735165951
DaehunGwak <rhkrgns56@naver.com>
2024-09-16 04:31:29 +0900

    feat: Add default dependencies & Modify views

--- app.py
+++ app.py
@@ -10,16 +10,14 @@ if "messages" not in st.session_state:
     init_message_on_session()
 
 st.set_page_config(
-    page_title="DocumentGPT",
-    page_icon="📃",
+    page_title="OpenAI Assistants - Search : Ordi",
+    page_icon="🔍",
 )
-st.title("DocumentGPT")
-st.markdown("""
----
-### Welcome!
-- Use this chatbot to ask questions  to an AI about your files
-- Upload your file on the sidebar.
----
+st.title("🔍 OpenAI Assistants")
+st.info("""
+### 👋 Welcome! 
+- Input your OpenAI key on the sidebar.
+- Use this agents to search with DuckDuckGo & Wikipedia
 """)
 
 ctx = get_script_run_ctx()
@@ -27,7 +25,6 @@ ctx = get_script_run_ctx()
 with st.sidebar:
     st.subheader("Inputs")
     input_api_key = st.text_input("Input your api key")
-    file = st.file_uploader("Upload your .txt file", type=['txt'])
 
     st.subheader("Features")
     if st.button("Clear your chat histories"):
@@ -39,9 +36,7 @@ with st.sidebar:
     st.write(f"api key: {input_api_key}")
 
 
-if file and input_api_key:
-    retriever = get_cached_retriever_after_embedding(file, ctx.session_id, input_api_key)
-
+if input_api_key:
     if len(st.session_state["messages"]) <= 0:
         save_message_on_session("i'm ready! Ask away!", "ai")
     paint_message_history()
@@ -50,10 +45,6 @@ if file and input_api_key:
     if input_message:
         save_message_on_session(input_message, "human")
         paint_message(input_message, "human")
-
-        chain = create_chain(retriever, input_api_key)
-        with st.chat_message("ai"):
-            response = chain.invoke(input_message)
 
 else:
     init_message_on_session()
